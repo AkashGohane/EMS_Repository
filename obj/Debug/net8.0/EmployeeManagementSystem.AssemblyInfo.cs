ae0c24deb237e37bcff9b0d0a04d56ef4e610ee4
AkashGohane <aakashgohane@gmail.com>
2024-06-09 17:53:03 +0530

    Generated files commit

--- obj/Debug/net8.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763bf1aeaf61c0650972edfb03eacae058ce4268")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
